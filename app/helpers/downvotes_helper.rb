e85d93697d13f8063eabb6b8e99d6c219d6762dc
Sudharshan <sudhar.ti@gmail.com>
2013-12-14 20:26:44 +0530

    Issue with vote downvote fixed

--- app/helpers/downvotes_helper.rb
+++ app/helpers/downvotes_helper.rb
@@ -1,2 +1,5 @@
 module DownvotesHelper
+	def user_has_downvoted?(id)
+		downvote = current_user.downvotes.find_by_downvotable_id(id)
+	end
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,7 @@
 module VotesHelper
+
+	def user_has_voted?(id)
+		vote = current_user.votes.find_by_votable_id(id) 
+	end
+
 end
